3318b823bdd574958115e9e932a5c2393df73aa6
DavidAlvarez09 <138899569+WebWizDave@users.noreply.github.com>
2026-03-30 15:43:04 -0500

    Clases basicas

---
+++ game/logic/arbol_avl.py
@@ -0,0 +1,13 @@
+class ArbolAVL:
+    def __init__(self):
+        self.raiz = None
+
+    def obtener_altura(self, nodo):
+        if not nodo:
+            return 0
+        return nodo.altura
+
+    def obtener_balance(self, nodo):
+        if not nodo:
+            return 0
+        return self.obtener_altura(nodo.izquierdo) - self.obtener_altura(nodo.derecho)
