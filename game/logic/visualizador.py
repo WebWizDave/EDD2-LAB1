64f9ce188cc789249fac14dd9bf9d6f0b1c356d1
DavidAlvarez09 <138899569+WebWizDave@users.noreply.github.com>
2026-04-03 16:27:24 -0500

    prueba creacion de grafo con graphviz

--- game/logic/visualizador.py
+++ game/logic/visualizador.py
@@ -4,26 +4,26 @@ import os
 class VisualizadorArbol:
     def __init__(self, arbol):
         self.arbol = arbol
-        # Creamos el objeto de Graphviz
+        # crear el objeto de graphviz
         self.dot = Digraph(comment='Sistema de Investigacion AVL')
-        # Personalización visual (Colores de la policía/detective)
+        # colores
         self.dot.attr('node', shape='record', style='filled', color='skyblue')
 
     def generar_imagen(self, nombre_archivo="arbol_casos"):
-        self.dot.clear() # Limpiamos para no duplicar nodos
+        self.dot.clear()
         self._construir_grafo(self.arbol.raiz)
         
-        # Definimos la ruta de salida (Carpeta images de Ren'Py)
+        # la ruta de salida carpeta images
         ruta_salida = os.path.join('game', 'images', nombre_archivo)
         
-        # Renderizamos la imagen (formato png)
+        # renderizar la imagen a formato png
         self.dot.render(ruta_salida, format='png', cleanup=True)
         print(f"Imagen generada en: {ruta_salida}.png")
 
     def _construir_grafo(self, nodo):
         if nodo:
-            # 1. Creamos el nodo visual con Info del Caso
-            # Usamos el ID y el Tipo de delito para la etiqueta
+            # 1. creamos el nodo con la informacion del caso
+            #  usamos el ID y el tipo de delito para la etiqueta de los nodos
             etiqueta = f"ID: {nodo.dato.id_caso} | {nodo.dato.tipo}\nGravedad: {nodo.dato.gravedad}"
             self.dot.node(str(nodo.dato.id_caso), etiqueta)
 
