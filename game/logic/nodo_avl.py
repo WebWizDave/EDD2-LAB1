ae81116ddf25d8b06665e9dac005ff3955bec262
DavidAlvarez09 <138899569+WebWizDave@users.noreply.github.com>
2026-05-14 16:34:36 -0500

    cambios clase nodo

--- game/logic/nodo_avl.py
+++ game/logic/nodo_avl.py
@@ -1,6 +1,8 @@
-class NodoAVL:
-    def __init__(self, caso):
-        self.dato = caso       
-        self.izquierdo = None    
-        self.derecho = None      
-        self.altura = 1
+class Nodo:
+    def __init__(self, dato, peso_real=1, descripcion=""):
+        self.dato = dato           # Este es el valor del delito (para posición)
+        self.peso_real = peso_real   # Este es el peso oculto que afecta el balance
+        self.descripcion = descripcion
+        self.izquierda = None
+        self.derecha = None
+        self.altura = peso_real    # Iniciamos la altura con el peso de la pista
